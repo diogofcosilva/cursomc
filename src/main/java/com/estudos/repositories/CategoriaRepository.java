a622dba97e249827c7c9d8308a9589dda5e35445
Diogo Silva <diogo.fransil@gmail.com>
2020-02-26 14:08:03 -0300

    save point

--- src/main/java/com/estudos/repositories/CategoriaRepository.java
+++ src/main/java/com/estudos/repositories/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package com.estudos.repositories;
 
-public class CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estudos.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
 }
